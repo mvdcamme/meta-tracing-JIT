732af2fdb3953b627e3c6c440645cc89097ba5ea
mvdcamme <maarten_vandercammen@hotmail.com>
2014-09-23 11:40:40 +0200

    Added loop annotations
    Added loop annotations to the SLIP meta-interpreter. Removed some
    debugging code.

--- slip tracing meta-interpreter.scm
+++ slip tracing meta-interpreter.scm
@@ -31,6 +31,8 @@
 
 (define meta-level-apply apply)
 
+(define list-of-expressions '())
+
 (begin
   (define meta-level-eval eval)
 
@@ -39,16 +41,16 @@
 ;
 
   (define (wrap-native-procedure native-procedure)
-    (lambda (arguments continue environment tailcall)
+    (lambda (arguments continue environment tailcall tracing?)
       (define native-value (meta-level-apply native-procedure arguments))
       (continue native-value environment)))
 
-  (define (cps-apply expression continue environment tailcall)
+  (define (cps-apply expression continue environment tailcall tracing?)
     (define procedure (car expression))
     (define arguments (cadr expression))
-    (procedure arguments continue environment tailcall))
+    (procedure arguments continue environment tailcall tracing?))
 
-  (define (cps-map expression continue environment tailcall)
+  (define (cps-map expression continue environment tailcall tracing?)
     (define procedure (car expression))
     (define items (cadr expression))
     (define (iterate items values)
@@ -58,10 +60,10 @@
               (tail (cdr items)))
           (define (continue-after-item value environment)
             (iterate tail (cons value values)))
-          (procedure (list head) continue-after-item environment #f))))
+          (procedure (list head) continue-after-item environment #f tracing?))))
     (iterate items '()))
 
- (define (cps-for-each expression continue environment tailcall)
+ (define (cps-for-each expression continue environment tailcall tracing?)
     (define procedure (car expression))
     (define items (cadr expression))
     (define (iterate items)
@@ -71,14 +73,14 @@
               (tail (cdr items)))
           (define (continue-after-item value environment)
             (iterate tail))
-          (procedure (list head) continue-after-item environment #f))))
+          (procedure (list head) continue-after-item environment #f tracing?))))
     (iterate items))
 
   (define (cps-call-cc expression continue environment tailcall)
     (define procedure (car expression))
     (define (continuation arguments dynamic-continue dynamic-environment tailcall)
       (continue (car arguments) environment))
-    (procedure (list continuation) continue environment tailcall))
+    (procedure (list continuation) continue environment tailcall tracing?))
 
 ;
 ; read-eval-print
@@ -113,24 +115,23 @@
             (bind-parameters (cdr parameters) (cdr arguments) environment))
           environment)))
 
-    (define (evaluate-sequence expressions continue environment tailcall)
+    (define (evaluate-sequence expressions continue environment tailcall tracing?)
       (define head (car expressions))
       (define tail (cdr expressions))
       (define (continue-with-sequence value environment)
-        (evaluate-sequence tail continue environment tailcall))
+        (evaluate-sequence tail continue environment tailcall tracing?))
       (if (null? tail)
-        (eval head continue environment tailcall)
-        (eval head continue-with-sequence environment #f)))
+        (eval head continue environment tailcall tracing?)
+        (eval head continue-with-sequence environment #f tracing?)))
 
     (define (make-procedure parameters expressions environment)
-      (lambda (arguments continue dynamic-environment tailcall)
+      (lambda (arguments continue dynamic-environment tailcall tracing?)
         (define (continue-after-sequence value environment-after-sequence)
           (continue value dynamic-environment))
         (define lexical-environment (bind-parameters parameters arguments environment))
-        (can-start-loop expressions)
         (if tailcall
-          (evaluate-sequence expressions continue lexical-environment #t)
-          (evaluate-sequence expressions continue-after-sequence lexical-environment #t))))
+          (evaluate-sequence expressions continue lexical-environment #t tracing?)
+          (evaluate-sequence expressions continue-after-sequence lexical-environment #t tracing?))))
 
 ;
 ; evaluation functions
@@ -138,62 +139,77 @@
 
     (define (evaluate-application operator)
       (lambda operands
-        (lambda (continue environment tailcall)
+        (lambda (continue environment tailcall tracing?)
           (define (continue-after-operator procedure environment-after-operator)
             (define (evaluate-operands operands arguments environment)
               (define (continue-with-operands value environment-with-operands)
                 (evaluate-operands (cdr operands) (cons value arguments) environment-with-operands))
               (if (null? operands)
-                (procedure (reverse arguments) continue environment tailcall)
-                (eval (car operands) continue-with-operands environment #f)))
+                (procedure (reverse arguments) continue environment tailcall tracing?)
+                (eval (car operands) continue-with-operands environment #f tracing?)))
             (evaluate-operands operands '() environment-after-operator))
-          (eval operator continue-after-operator environment #f))))
+          (if tracing?
+              (begin (display "(") (display operator) (display " ") (display operands) (display ")")
+                     (newline)))
+          (eval operator continue-after-operator environment #f tracing?))))
 
     (define (evaluate-begin . expressions)
-      (lambda (continue environment tailcall)
-        (evaluate-sequence expressions continue environment tailcall)))
+      (lambda (continue environment tailcall tracing?)
+        (evaluate-sequence expressions continue environment tailcall tracing?)))
+    
+    (define (evaluate-can-start-loop expression)
+      (lambda (continue environment tailcall tracing?)
+        (define (continue-after-expression expression environment-after-continuation)
+          (if (member expression list-of-expressions)
+              (begin (display "looping!!!") (newline)
+                     (continue '() environment))
+              (begin (set! list-of-expressions (cons expression list-of-expressions))
+                     (continue '() environment))))
+        (display list-of-expressions) (newline)
+        (display "number of distinct expressions: ") (display (length list-of-expressions)) (newline)
+        (eval expression continue-after-expression environment tailcall tracing?)))
     
     (define (evaluate-cond . expressions)
-      (lambda (continue environment tailcall)
+      (lambda (continue environment tailcall tracing?)
         (define (evaluate-expressions expressions)
           (define (continue-after-predicate boolean environment-after-predicate)
             (if (eq? boolean #f)
                 (evaluate-expressions (cdr expressions))
-                (evaluate-sequence (cdar expressions) continue environment-after-predicate tailcall)))
+                (evaluate-sequence (cdar expressions) continue environment-after-predicate tailcall tracing?)))
           (cond ((null? expressions) (continue '() environment))
-                ((eq? (caar expressions) 'else) (evaluate-sequence (cdar expressions) continue environment tailcall))
-                (else (eval (caar expressions) continue-after-predicate environment #f))))
+                ((eq? (caar expressions) 'else) (evaluate-sequence (cdar expressions) continue environment tailcall tracing?))
+                (else (eval (caar expressions) continue-after-predicate environment #f tracing?))))
         (evaluate-expressions expressions)))
 
     (define (evaluate-define pattern . expressions)
-      (lambda (continue environment tailcall)
+      (lambda (continue environment tailcall tracing?)
         (define (continue-after-expression value environment-after-expression)
           (define binding (cons pattern value))
           (continue value (cons binding environment-after-expression)))
         (if (symbol? pattern)
-          (eval (car expressions) continue-after-expression environment #f)
+          (eval (car expressions) continue-after-expression environment #f tracing?)
           (let* ((binding (cons (car pattern) '()))
                  (environment (cons binding environment))
                  (procedure (make-procedure (cdr pattern) expressions environment)))
               (set-cdr! binding procedure)
               (continue procedure environment)))))
 
-    (define (evaluate-if predicate consequent . alternative)
-      (lambda (continue environment tailcall)
+    (define (evaluate-if predicate consequent . alternative) ;beware of guards
+      (lambda (continue environment tailcall tracing?)
         (define (continue-after-predicate boolean environment-after-predicate)
           (if (eq? boolean #f) 
             (if (null? alternative)
               (continue '() environment-after-predicate)
-              (eval (car alternative) continue environment-after-predicate tailcall))
-            (eval consequent continue environment-after-predicate tailcall)))
-        (eval predicate continue-after-predicate environment #f)))
+              (eval (car alternative) continue environment-after-predicate tailcall tracing?))
+            (eval consequent continue environment-after-predicate tailcall tracing?)))
+        (eval predicate continue-after-predicate environment #f tracing?)))
 
     (define (evaluate-lambda parameters . expressions)
-      (lambda (continue environment tailcall)
+      (lambda (continue environment tailcall tracing?)
         (continue (make-procedure parameters expressions environment) environment)))
     
     (define (evaluate-let* . expressions)
-      (lambda (continue environment tailcall)
+      (lambda (continue environment tailcall tracing?)
         (let* ((bindings (car expressions))
                (body (cdr expressions)))
           (define (evaluate-bindings bindings environment)
@@ -203,29 +219,23 @@
                      (new-environment (cons binding environment)))
                 (evaluate-bindings (cdr bindings) new-environment)))
             (if (null? bindings)
-                (evaluate-sequence body continue environment tailcall)
-                (eval (cadar bindings) continue-after-binding environment #f)))
+                (evaluate-sequence body continue environment tailcall tracing?)
+                (eval (cadar bindings) continue-after-binding environment #f tracing?)))
           (evaluate-bindings bindings environment))))
 
     (define (evaluate-quote expression)
-      (lambda (continue environment tailcall)
+      (lambda (continue environment tailcall tracing?)
         (continue expression environment)))
 
     (define (evaluate-set! variable expression)
-      (lambda (continue environment tailcall)
+      (lambda (continue environment tailcall tracing?)
         (define (continue-after-expression value environment-after-expression)
           (define binding (assoc variable environment-after-expression))
           (if binding
             (set-cdr! binding value)
             (error "inaccessible variable: " variable))
           (continue value environment-after-expression))
-        (eval expression continue-after-expression environment #f)))
-    
-    (define (wrap-native-eval)
-      (lambda (arguments continue environment tailcall)
-        (define expression (car arguments))
-        (define native-value (meta-level-eval expression (interaction-environment)))
-        (continue native-value environment)))
+        (eval expression continue-after-expression environment #f tracing?)))
 
     (define (evaluate-variable variable continue environment)
       (define binding (assoc variable environment))
@@ -236,23 +246,22 @@
                         (continue native-value environment))))))
 
     (define (evaluate-while predicate . expressions)
-      (lambda (continue environment tailcall)
+      (lambda (continue environment tailcall tracing?)
         (define (iterate value environment)
           (define (continue-after-predicate boolean environment-after-predicate)
             (define (continue-after-sequence value environment-after-sequence)
               (iterate value environment))
             (if (eq? boolean #f)
               (continue value environment)
-              (evaluate-sequence expressions continue-after-sequence environment #f)))
-          (eval predicate continue-after-predicate environment #f))
-        (can-start-loop expressions)
+              (evaluate-sequence expressions continue-after-sequence environment #f tracing?)))
+          (eval predicate continue-after-predicate environment #f tracing?))
         (iterate '() environment)))
 
 ;
 ; evaluator
 ;
   
-    (define (evaluate expression continue environment tailcall)
+    (define (evaluate expression continue environment tailcall tracing?)
       (cond
         ((symbol? expression)
          (evaluate-variable expression continue environment))
@@ -261,6 +270,7 @@
                (operands (cdr expression)))
            ((apply
              (cond ((eq? operator 'begin) evaluate-begin)
+                   ((eq? operator 'can-start-loop) evaluate-can-start-loop)
                    ((eq? operator 'cond) evaluate-cond)
                    ((eq? operator 'define) evaluate-define)
                    ((eq? operator 'if) evaluate-if)
@@ -269,7 +279,7 @@
                    ((eq? operator 'quote) evaluate-quote)
                    ((eq? operator 'set!) evaluate-set!)
                    ((eq? operator 'while) evaluate-while)
-                   (else (evaluate-application operator))) operands) continue environment tailcall)))
+                   (else (evaluate-application operator))) operands) continue environment tailcall tracing?)))
         (else
           (continue expression environment))))
 
@@ -282,7 +292,7 @@
     (display output)
     (newline)
     (display ">>>")
-    (eval (read) loop environment #f))
+    (eval (read) loop environment #f #f))
 
   (loop "cpSlip* version 3" (list (cons 'apply    cps-apply   ) 
                                   (cons 'map      cps-map     ) 
