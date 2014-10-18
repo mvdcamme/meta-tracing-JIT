515c8eeee9b960aaa5955450aa81ec10b2b27425
mvdcamme <maarten_vandercammen@hotmail.com>
2014-10-18 15:20:36 +0200

    Deleted cpSlip.scm; Added other SLIP meta-interpreter
    cpSlip had become redundant.
    The recursive SLIP meta-interpreter is a new SLIP meta-interpreter which just uses simple recursion to evaluate SLIP programs. It allows us to test our meta-tracing on more than one interpreter.

---
+++ recursive slip meta-interpreter.scm
@@ -0,0 +1,149 @@
+(begin
+  
+  (define (assocv el lst)
+    (cond ((null? lst) #f)
+          ((eq? (vector-ref (car lst) 0) el) (car lst))
+          (else (assocv el (cdr lst)))))
+  
+  (define (map f lst)
+    (if (null? lst)
+        '()
+        (cons (f (car lst)) (map f (cdr lst)))))
+  
+  (define meta-circularity-level 0)
+  (define (loop environment output)
+    (define rollback environment)
+    
+    (define (evaluate expression)
+      
+      (define (abort message qualifier)
+        (display message)
+        (loop rollback qualifier))
+      
+      (define (bind-variable variable value)
+        (define binding (vector variable value))
+        (set! environment (cons binding environment)))
+      
+      (define (bind-parameters parameters arguments)
+        (if (symbol? parameters)
+            (bind-variable parameters arguments)
+            (if (pair? parameters)
+                (let* ((variable (car parameters))
+                      (value (car arguments)))
+                  (bind-variable variable value)
+                  (bind-parameters (cdr parameters) (cdr arguments))))))
+      
+      (define (thunkify expression)
+        (define frozen-environment environment)
+        (define value (evaluate expression))
+        (set! environment frozen-environment)
+        value)
+      
+      (define (evaluate-sequence expressions)
+        (define head (car expressions))
+        (define tail (cdr expressions))
+        (let* ((value (evaluate head)))
+          (if (null? tail)
+              value
+              (evaluate-sequence tail))))
+      
+      (define (close parameters expressions)
+        (define lexical-environment environment)
+        (define (closure . arguments)
+          (define dynamic-environment environment)
+          (can-start-loop expressions "some function")
+          (set! environment lexical-environment)
+          (bind-parameters parameters arguments)
+          (let* ((value (evaluate-sequence expressions)))
+            (set! environment dynamic-environment)
+            value))
+        closure)
+      
+      (define (evaluate-application operator)
+        (lambda operands
+          (apply (evaluate operator) (map evaluate operands))))
+      
+      (define (evaluate-begin . expressions)
+        (evaluate-sequence expressions))
+      
+      (define (evaluate-define pattern . expressions)
+        (if (symbol? pattern)
+            (let* ((value (evaluate (car expressions)))
+                   (binding (vector pattern value)))
+              (set! environment (cons binding environment))
+              value)
+            (let* ((binding (vector (car pattern) '())))
+              (set! environment (cons binding environment))
+              (let* ((closure (close (cdr pattern) expressions)))
+                (vector-set! binding 1 closure)
+                closure))))
+      
+      (define (evaluate-eval expression)
+        (evaluate (evaluate expression)))
+      
+      (define (evaluate-if predicate consequent . alternate)
+        (if (evaluate predicate)
+            (thunkify consequent)
+            (if (null? alternate)
+                '()
+                (thunkify (car alternate)))))
+      
+      (define (evaluate-lambda parameters . expressions)
+        (close parameters expressions))
+      
+      (define (evaluate-load string)
+        (let* ((port (open-input-file string))
+               (exp (read-port)))
+          (close-input-port port)
+          (evaluate (read port))))
+      
+      (define (evaluate-quote expression)
+        expression)
+      
+      (define (evaluate-set! variable expression)
+        (define value (evaluate expression))
+        (define binding (assocv variable environment))
+        (if (vector? binding)
+            (vector-set! binding 1 value)
+            (abort "inaccessible variable: " variable)))
+      
+      (define (evaluate-variable variable)
+        (define binding (assocv variable environment))
+        (if (vector? binding)
+            (vector-ref binding 1)
+            (eval variable (make-base-namespace))))
+      
+      (cond
+        ((symbol? expression)
+         (evaluate-variable expression))
+        ((pair? expression)
+         (let* ((operator (car expression))
+               (operands (cdr expression)))
+           (apply
+            (cond ((eq? operator 'begin)
+                   evaluate-begin)
+                  ((eq? operator 'define) 
+                   evaluate-define)
+                  ((eq? operator 'eval)
+                   evaluate-eval)
+                  ((eq? operator 'if) 
+                   evaluate-if)
+                  ((eq? operator 'lambda) 
+                   evaluate-lambda)
+                  ((eq? operator 'load) 
+                   evaluate-load)
+                  ((eq? operator 'quote) 
+                   evaluate-quote)
+                  ((eq? operator 'set!) 
+                   evaluate-set!)
+                  (else
+                   (evaluate-application operator))) operands)))
+        (else
+         expression)))
+    
+    (display output)
+    (newline)
+    (display ">>>")
+    (loop environment (evaluate (read))))
+  
+  (loop '() "Slip"))
