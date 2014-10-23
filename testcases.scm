0cd189e8837371a4ad667c88c3e789be425354c1
mvdcamme <maarten_vandercammen@hotmail.com>
2014-10-23 21:51:10 +0200

    Added hanoi testcase

--- testcases.scm
+++ testcases.scm
@@ -121,4 +121,68 @@
       n
       (+ (fib (- n 1)) (fib (- n 2)))))
 
+;8
+;regular-trace/meta-trace
+
+;
+; Towers of Hanoi (source: http://www.cems.uvm.edu/~rsnapp/teaching/cs32/src/hanoi.rkt)
+;
+
+(begin
+
+(define (make-list n value)
+    (if (zero? n) 
+        '()
+        (cons value (make-list (- n 1) value))))
+
+(define (remove-all s lst) 
+    (cond ((null? lst) '()); an empty list
+          ((eq? s (car lst)) (remove-all s (cdr lst)))
+          (else (cons (car lst) (remove-all s (cdr lst))))))
+
+(define (initial-state n)
+    (make-list n 1))
+
+(define (move-check? src-peg dst-peg state)
+    (cond ((null? state) #f) ; There is no disk on peg (first m)!
+          ((eq? (car state) src-peg) #t)
+          ((eq? (car state) dst-peg) #f)
+          (else (move-check? src-peg dst-peg (cdr state)))))
+
+(define (move-disk src-peg dst-peg state)
+    (cond ((null? state) '())
+          ((eq? (car state) dst-peg) state) ; Move is illegal!
+          ((eq? (car state) src-peg) (cons dst-peg (cdr state))) ; Move is legal.
+          (else (cons (car state) (move-disk src-peg dst-peg (cdr state))))))
+
+(define (sequential-move move-list state)
+    (if (null? move-list) ; Is the move-list empty?
+        state
+        (let* ((next-move (car move-list))
+               (src-peg (car next-move))
+               (dst-peg (cadr next-move))
+               (next-state (move-disk src-peg dst-peg state)))
+          (if (move-check? src-peg dst-peg state)
+              (begin
+                (printf "Moving smallest disk from peg ~s onto peg ~s in state ~s yields state ~s.\n" 
+                        src-peg dst-peg state next-state)
+                (sequential-move (cdr move-list) next-state))
+              (error 'sequential-move "Illegal move.")
+              ))))
+
+(define (agent home-peg target-peg n)
+  (let ((spare-peg (car (remove-all home-peg (remove-all target-peg '(1 2 3))))))
+    (if (<= n 0)
+        '() ; an empty list
+        (append (agent home-peg spare-peg (- n 1))
+                (list (list home-peg target-peg))
+                (agent spare-peg target-peg (- n 1))))))
+
+(define (hanoi n)
+  (let ((state (initial-state n)))
+    (printf "Initial state: ~s.\n" state)
+    (sequential-move (agent 1 3 n) state)))
+
+(hanoi 5))
+
 |#
