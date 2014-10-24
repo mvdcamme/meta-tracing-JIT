fed4543109638219a2643c5b3ab2d6c7b9ca61cf
mvdcamme <maarten_vandercammen@hotmail.com>
2014-10-25 00:00:15 +0200

    Added bubblesort testcase

--- testcases.scm
+++ testcases.scm
@@ -185,4 +185,28 @@
 
 (hanoi 5))
 
+;9
+;regular-trace/meta-trace
+
+(define (bubble-sort vector <<?)  
+   (define (bubble-swap vector idx1 idx2)
+     (let ((keep (vector-ref vector idx1)))
+       (vector-set! vector idx1 (vector-ref vector idx2))
+       (vector-set! vector idx2 keep)
+       #t))
+   (let outer-loop
+     ((unsorted-idx (- (vector-length vector) 2)))
+     (if (>= unsorted-idx 0)
+       (if (let inner-loop
+             ((inner-idx 0)
+              (has-changed? #f))
+             (if (> inner-idx unsorted-idx)
+               has-changed?
+               (inner-loop (+ inner-idx 1)
+                           (if (<<? (vector-ref vector (+ inner-idx 1))
+                                    (vector-ref vector inner-idx))
+                             (bubble-swap vector inner-idx (+ inner-idx 1))
+                             has-changed?))))
+         (outer-loop (- unsorted-idx 1))))))
+
 |#
