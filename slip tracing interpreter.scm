a8075ea65498e2c0764b079da5a3a0b49fd88b73
mvdcamme <maarten_vandercammen@hotmail.com>
2014-12-01 10:36:18 +0100

    Added save-all-vals instruction for calls to 'apply'
    Previously, the save-vals i instruction was used but this caused some bugs when the i that should be used differed from the one in the trace

--- slip tracing interpreter.scm
+++ slip tracing interpreter.scm
@@ -426,6 +426,9 @@
   (set! θ (append (take v i) θ))
   (set! v (drop v i)))
 
+(define (save-all-vals)
+  (set! θ (append v θ)))
+
 (define (save-env)
   (set! θ (cons ρ θ)))
 
@@ -652,7 +655,7 @@
      (ko (car κ) (cdr κ)))
     ((ko (applyk rator) κ)
      (let ((i (length v)))
-       (execute `(save-vals ,i)
+       (execute `(save-all-vals)
                 `(save-env)
                 `(add-continuation ,(ratork i 'apply)))
        (ev rator (cons (ratork i 'apply) κ))))
