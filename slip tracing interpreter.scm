c6b0e9a7415d103dd31abdfed871386c08b53bc4
mvdcamme <maarten_vandercammen@hotmail.com>
2014-12-14 15:34:41 +0100

    Refactored random function so that it either creates a random number based on an existing, hardcoded, pseudo-random-generator, or that uses the current Racket pseudo-random-generator, depending on the value of the IS_DEBUGGING flag

--- slip tracing interpreter.scm
+++ slip tracing interpreter.scm
@@ -68,6 +68,7 @@
   
   (define ENABLE_OPTIMIZATIONS #f)
   (define ENABLE_OUTPUT #f)
+  (define IS_DEBUGGING #t)
   (define TRACING_THRESHOLD 5)
   
   (define ns (make-base-namespace))
@@ -162,15 +163,22 @@
   ; predefined functions
   ;
   
-  (define PSEUDO_RANDOM_GENERATOR_STATE '#(1842099444 2669855047 116122093 2587168346 679409090 3792812305))
+  (define PSEUDO_RANDOM_GENERATOR_STATE '#(2816165110 2738388292 45348490 3966956132 40780214 47365848))
   (define PSEUDO_RANDOM_GENERATOR (vector->pseudo-random-generator PSEUDO_RANDOM_GENERATOR_STATE))
   
-  (define pseudo-random-address 'u-pseudo-rand)
+  (define meta-random-address 'u-meta-rand)
   (define pseudo-random-generator-address 'u-pseudo-rand-gen)
   
   (define pseudo-random (clo (lam '(max)
-                                  '((random max pseudo-random-generator)))
+                                  `((random max ,PSEUDO_RANDOM_GENERATOR)))
                              (env `((pseudo-random-generator . ,pseudo-random-generator-address)))))
+  (define regular-random (clo (lam '(max)
+                                   '((random max)))
+                              (env '())))
+  
+  (define meta-random (if IS_DEBUGGING
+                          pseudo-random
+                          regular-random))
   
   ;
   ;tracing
@@ -1014,7 +1022,7 @@
     (ev e `(,(haltk))))
   
   (define (make-new-env)
-    (env `((random . ,pseudo-random-address))))
+    (env `((random . ,meta-random-address))))
   
   (define (add-var-to-env old-env var adr)
     (let ((old-lst (env-lst old-env)))
