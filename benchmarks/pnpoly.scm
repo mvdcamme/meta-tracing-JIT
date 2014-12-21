ae5e5bf142a2a500d3b4c4bb651983aed56d20c0
mvdcamme <maarten_vandercammen@hotmail.com>
2014-12-21 23:04:13 +0100

    Converted to proper syntax
    Changed let to let* and desugared named let

--- benchmarks/pnpoly.scm
+++ benchmarks/pnpoly.scm
@@ -1,31 +1,32 @@
 ;;; PNPOLY - Test if a point is contained in a 2D polygon.
 
 (begin
-	(define (pt-in-poly2 xp yp x y)
-	  (let loop ((c #f) (i (- (length xp) 1)) (j 0))
-	    (if (< i 0)
-	      c
-	      (if (or (and (or (> (list-ref yp i) y)
-	                       (>= y (list-ref yp j)))
-	                   (or (> (list-ref yp j) y)
-	                       (>= y (list-ref yp i))))
-	              (>= x
-	                       (+ (list-ref xp i)
-	                               (/ (*
-	                                        (- (list-ref xp j)
-	                                                (list-ref xp i))
-	                                        (- y (list-ref yp i)))
-	                                       (- (list-ref yp j)
-	                                               (list-ref yp i))))))
-	        (loop c (- i 1) i)
-	        (loop (not c) (- i 1) i)))))
+  (define (pt-in-poly2 xp yp x y)
+    (define (loop c i j)
+      (if (< i 0)
+          c
+          (if (or (and (or (> (list-ref yp i) y)
+                           (>= y (list-ref yp j)))
+                       (or (> (list-ref yp j) y)
+                           (>= y (list-ref yp i))))
+                  (>= x
+                      (+ (list-ref xp i)
+                         (/ (*
+                             (- (list-ref xp j)
+                                (list-ref xp i))
+                             (- y (list-ref yp i)))
+                            (- (list-ref yp j)
+                               (list-ref yp i))))))
+              (loop c (- i 1) i)
+              (loop (not c) (- i 1) i))))
+    (loop #f (- (length xp) 1) 0))
 	
 	(define (run)
-	  (let ((count 0)
-	        (xp '(0. 1. 1. 0. 0. 1. -.5 -1. -1. -2. -2.5
-	                               -2. -1.5 -.5 1. 1. 0. -.5 -1. -.5))
-	        (yp '(0. 0. 1. 1. 2. 3. 2. 3. 0. -.5 -1.
-	                               -1.5 -2. -2. -1.5 -1. -.5 -1. -1. -.5)))
+	  (let* ((count 0)
+                 (xp '(0. 1. 1. 0. 0. 1. -.5 -1. -1. -2. -2.5
+                          -2. -1.5 -.5 1. 1. 0. -.5 -1. -.5))
+                 (yp '(0. 0. 1. 1. 2. 3. 2. 3. 0. -.5 -1.
+                          -1.5 -2. -2. -1.5 -1. -.5 -1. -1. -.5)))
 	    (if (pt-in-poly2 xp yp .5 .5) (set! count (+ count 1)))
 	    (if (pt-in-poly2 xp yp .5 1.5) (set! count (+ count 1)))
 	    (if (pt-in-poly2 xp yp -.5 1.5) (set! count (+ count 1)))
