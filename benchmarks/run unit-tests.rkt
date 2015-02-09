439278da42e436a3953ff1c76a5cbc8c5fd22a5f
mvdcamme <maarten_vandercammen@hotmail.com>
2015-02-09 21:57:24 +0100

    Bugfix
    Forgot to add argument

--- benchmarks/run unit-tests.rkt
+++ benchmarks/run unit-tests.rkt
@@ -3,11 +3,11 @@
   (require (file "benchmark-paths.rkt"))
   (require (file "run benchmarks.rkt"))
   
-  (run-benchmarks (list fac-benchmark-path
-                        fib-benchmark-path
-                        rsa-benchmark-path
-                        nqueens-benchmark-path
-                        rotate-benchmark-path
-                        simplified-trace-explosion-benchmark-path
-                        scheme2c-benchmark-path
-                        simple-guard-trace-merging-benchmark-path)))
+  (run-benchmarks 1 (list fac-benchmark-path
+                          fib-benchmark-path
+                          rsa-benchmark-path
+                          nqueens-benchmark-path
+                          rotate-benchmark-path
+                          simplified-trace-explosion-benchmark-path
+                          scheme2c-benchmark-path
+                          simple-guard-trace-merging-benchmark-path)))
