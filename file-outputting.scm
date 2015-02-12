1bfe4194ecbec2c8849c2b23e17d8968bd8ee75c
mvdcamme <maarten_vandercammen@hotmail.com>
2015-02-13 00:24:05 +0100

    When using output files, the current-milliseconds are appended to the name of that output file

--- file-outputting.scm
+++ file-outputting.scm
@@ -7,14 +7,15 @@
   (require racket/date)
   
   (define (make-full-output-file-name base-file-name base-extension)
-    (let ((name-datetime-separator " "))
+    (let ((name-datetime-separator " ")
+          (milliseconds (modulo (current-milliseconds) 1000)))
       (define (make-datetime-file-name-part)
         (let* ((seconds (current-seconds))
                (date (seconds->date seconds #t))
                (colon-replace-string ""))
           (date-display-format 'iso-8601)
           (let ((basic-string (date->string date #t)))
-            (string-replace basic-string ":" colon-replace-string))))
+            (string-append (string-replace basic-string ":" colon-replace-string) "-" (number->string milliseconds)))))
       (string-append base-file-name name-datetime-separator (make-datetime-file-name-part) "." base-extension)))
   
   (define (output-element element port)
