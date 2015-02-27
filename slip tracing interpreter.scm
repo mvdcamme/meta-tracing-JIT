9050cb749dcd86ea17afa268c7585dc6566ffb30
mvdcamme <maarten_vandercammen@hotmail.com>
2015-02-27 22:44:44 +0100

    Added export

--- slip tracing interpreter.scm
+++ slip tracing interpreter.scm
@@ -14,6 +14,7 @@
            add-continuation
            alloc-var
            apply-native
+           call-global-continuation
            call-label-trace!
            create-closure
            debug
