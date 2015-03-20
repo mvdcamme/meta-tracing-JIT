34c22b7edcf57f56a4f035ca7ec5ac600bdbffd6
mvdcamme <maarten_vandercammen@hotmail.com>
2015-03-20 15:37:05 +0100

    Replaced generic ck-state struct by the specialized ev- and ko-structs

--- interaction.scm
+++ interaction.scm
@@ -8,12 +8,22 @@
            (struct-out is-evaluating-encountered))
   
   ;
-  ; Program state
+  ; CK wrappers
   ;
   
-  (struct ck (c ; control
-              κ ; continuation stack
-              ) #:transparent)
+  ;;; Represents the state of a program when evaluating an expression.
+  ;;; It consists of an expression to be evaluated (e), and a list of continuations to be followed
+  ;;; once the evaluation is complete (κ).
+  (struct ev (e κ) #:transparent)
+  
+  ;;; Represents the state of a program when following a continuation.
+  ;;; It consists of the continuation to be followed immediately (φ) and a list of continuations
+  ;;; to be followed afterwards (κ).
+  (struct ko (φ κ) #:transparent)
+  
+  ;
+  ; Program state
+  ;
   
   (struct program-state (ck
                          ρ ; env
