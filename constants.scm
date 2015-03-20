6f865dfe324e7d309769ae206a5737828009e834
mvdcamme <maarten_vandercammen@hotmail.com>
2015-03-20 15:18:32 +0100

    Added module for outputting expressions (e.g. printing them to the console)

--- constants.scm
+++ constants.scm
@@ -2,6 +2,10 @@
   
   (provide (all-defined-out))
   
+  ;;; Determines whether all 'output'-statements effectively print their argument to the console
+  ;;; or not.
+  (define ENABLE_OUTPUT #f)
+  
   ;;; Has the following effects:
   ;;;  - Provided the meta-traced interpreter uses the 'random' function defined on this level
   ;;;    (the tracing interpreter) when calling 'random' in the user-program, the random number
