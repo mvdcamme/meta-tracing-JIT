870b85715d3b8b9e103b3d97d6efaf3b8118fefe
mvdcamme <maarten_vandercammen@hotmail.com>
2015-03-21 19:55:20 +0100

    Moved continuations to separate module 'continuations.scm'

--- cesk-interpreter.scm
+++ cesk-interpreter.scm
@@ -6,39 +6,4 @@
            "interaction.scm"
            "output.scm")
   
-  ;
-  ; Continuations
-  ;
-  
-  (struct andk (es) #:transparent)
-  (struct apply-failedk (rator i) #:transparent)
-  (struct applicationk (debug) #:transparent)
-  (struct applyk (rator) #:transparent)
-  (struct closure-guard-failedk (i) #:transparent)
-  (struct condk (pes es) #:transparent)
-  (struct definevk (x) #:transparent)
-  (struct haltk () #:transparent)
-  (struct ifk (e1 e2) #:transparent)
-  (struct letk (x es) #:transparent)
-  (struct let*k (x bds es) #:transparent)
-  (struct letreck (x bds es) #:transparent)
-  (struct ork (es) #:transparent)
-  (struct randk (e es i) #:transparent)
-  (struct ratork (i debug) #:transparent)
-  (struct seqk (es) #:transparent)
-  (struct setk (x) #:transparent)
-  
-  ;
-  ; Tracing annotations continuations
-  ;
-  
-  ;;; The continuation to be followed after encountering a can-close-loop annotation.
-  (struct can-close-loopk () #:transparent)
-  
-  ;;; The continuation to be followed after encountering a can-start-loop annotation.
-  (struct can-start-loopk (label debug-info) #:transparent)
-  
-  ;;; The continuation to be followed after encountering a is-evaluating annotation.
-  (struct is-evaluatingk () #:transparent)
-  
   )
